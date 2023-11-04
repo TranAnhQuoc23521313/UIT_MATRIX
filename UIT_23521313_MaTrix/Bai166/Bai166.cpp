f8ef68019043f69ea4b4dfe7826633f4223ebefa
TranAnhQuoc23521313 <146335703+TranAnhQuoc23521313@users.noreply.github.com>
2023-11-04 14:28:51 +0700

    Update Bai166.cpp

--- UIT_23521313_MaTrix/Bai166/Bai166.cpp
+++ UIT_23521313_MaTrix/Bai166/Bai166.cpp
@@ -73,7 +73,7 @@ int KtCon(float a[][500], int m, int n, int vtd, int vtc)
 	int flag = 1;
 	for (int i = 0; i < 3; i++)
 		for (int j = 0; j < 3; j++)
-			if (a[vtd + i][vtc + i] <= 0)
+			if (a[vtd + i][vtc + j] <= 0)
 				flag = 0;
 	return flag;
 }
